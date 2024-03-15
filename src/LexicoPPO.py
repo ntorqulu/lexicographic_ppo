6e1580dc5d6f63981e1340aba4969fc6f398b152
Núria Torquet Luna <ntorqulu11@alumnes.ub.edu>
2024-03-15 18:23:33 +0100

    changes in updates for actor, critic and lagrange. Missing compute_loss for actor

--- src/LexicoPPO.py
+++ src/LexicoPPO.py
@@ -1,8 +1,18 @@
 import collections
+import torch.nn as nn
 
 from agent import make_network, Agent
 from src.utils.misc import *
-from torch.distributions import Categorical
+
+
+def _array_to_dict_tensor(agents: List[int], data: Array, device: th.device, astype: Type = th.float32) -> Dict:
+    # Check if the provided device is already the current device
+    is_same_device = (device == th.cuda.current_device()) if device.type == 'cuda' else (device == th.device('cpu'))
+
+    if is_same_device:
+        return {k: th.as_tensor(d, dtype=astype) for k, d in zip(agents, data)}
+    else:
+        return {k: th.as_tensor(d, dtype=astype).to(device) for k, d in zip(agents, data)}
 
 
 class LexicoPPO:
@@ -33,8 +43,8 @@ class LexicoPPO:
         self.private_callbacks = []
 
         # Training parameters
-        self.tot_steps = train_params.tot_steps # total steps done in training
-        self.max_ep_length = train_params.max_steps # max number of steps per episode is always 500
+        self.tot_steps = train_params.tot_steps  # total steps done in training
+        self.max_ep_length = train_params.max_steps  # max number of steps per episode is always 500
         self.batch_size = train_params.batch_size
 
         # Torch init
@@ -79,6 +89,9 @@ class LexicoPPO:
         self.o_size = self.env.observation_space.sample().shape[0]
         self.a_size = self.env.action_space.n
 
+    def environment_reset(self):
+        return _array_to_dict_tensor(self.r_agents, self.env.reset(), self.device)
+
     # Always reset and set_Agents to none for now
     def train(self):
         self.environment_setup()
@@ -91,32 +104,108 @@ class LexicoPPO:
         # num_trajectories = 2500 // 500 = 5
         self.n_updates = self.tot_steps // self.batch_size
         for update in range(1, self.n_updates + 1):
-            self.rollout() # collect trajectories
-            self.update() # update the policy
+            self.rollout()  # collect trajectories
+            self.update()  # update the policy
         self._finish_training()
 
     def rollout(self):
         observation = self.environment_reset()
         # set actions to 0 for all agents
         action = [0 for _ in self.r_agents]
-        env_action = [0 for _ in self.r_agents]
         for step in range(self.max_ep_length):
             with th.no_grad():
                 for k in self.r_agents:
-                    env_action[k] = self.agents[k].actor.get_action(observation[k])
-            non_tensor_next_observation, reward, done, info = self.env.step(env_action)
+                    action[k] = self.agents[k].actor.get_action(observation[k])
+            non_tensor_next_observation, reward, done, info = self.env.step(action)
+            reward = _array_to_dict_tensor(self.r_agents, reward, self.device)
+            done = _array_to_dict_tensor(self.r_agents, done, self.device)
             next_observation = _array_to_dict_tensor(self.r_agents, non_tensor_next_observation, self.device)
+            # TODO: check how to store rewards to set a preference
+            """
             if mode == 1:
                 # set first component of the reward positive and the second negative
                 reward = [[reward[k][0], -reward[k][1]] for k in self.r_agents]
-            else:
+            elif mode == 2:
                 # set first component of the reward negative and the second positive
                 reward = [[-reward[k][0], reward[k][1]] for k in self.r_agents]
+            # if mode == 3, let them unchanged
+            """
+            # For now, let rewards unchanged
+            # save the collected experience into the buffer
+            if not self.eval_mode:
+                for k in self.r_agents:
+                    self.buffer[k].store(
+                        observation[k],
+                        action[k],
+                        reward[k],
+                        next_observation[k],
+                        done[k]
+                    )
+            # end simulation
+            if all(list(done.values())):
+                observation = self.environment_reset()
 
     def update(self):
-        pass
+        for k in self.r_agents:
+            # reset index of the buffer
+            self.buffer[k].reset_index()
+            # sample the buffer
+            batch = self.buffer[k].sample()
+            # TODO: we don't do multiple epoch for the same batch
+            # for each agent, update the actor, the critic and the lagrange multipliers
+            # update the actor
+            self.update_actor(batch, k)
+            # update the critic
+            self.update_critic(batch, k)
+            # update lagrange multipliers
+            self.update_lagrange(batch, k)
 
     def _finish_training(self):
         pass
 
+    def update_actor(self, batch, k):
+        self.agents[k].actor().train()
+        actor_loss = self.compute_loss(batch, self.reward_size, k)
+        self.agents[k].a_optimizer.zero_grad()
+        actor_loss.backward()
+        self.agents[k].a_optimizer.step()
+        # eval
+
+    def update_critic(self, batch, k):
+        states, _, rewards, next_states, dones = batch
+        self.agents[k].critic().train()
+        predictions = self.agents[k].critic(states.to(self.device))
+        with th.no_grad():
+            target = rewards + (self.discount * self.agents[k].critic(next_states.to(self.device)) * (
+                    1 - dones))
+        critic_loss = nn.MSELoss()(predictions, target)
+        self.agents[k].c_optimizer.zero_grad()
+        critic_loss.backward()
+        self.agents[k].c_optimizer.step()
+        # eval
+
+    def update_lagrange(self, batch, k):
+        for i in range(self.reward_size - 1):
+            self.j[i] = -th.tensor(self.recent_losses[i][25:]).mean()
+        # update the lagrange multipliers
+        r = self.reward_size - 1
+        for i in range(r):
+            self.mu[i] += self.eta[i] * (self.j[i] - (-self.recent_losses[i][-1]))
+            self.mu[i] = max(0.0, self.mu[i])
+
+    def compute_loss(self, batch, reward_size, k):
+        states, actions, rewards, next_states, dones = batch
+        loss = "Undefined"
+        first_order = []
+        for i in range(reward_size - 1):
+            w = self.beta[i] * self.mu[i] * sum(self.beta[j] for j in range(i+1, reward_size))
+            first_order.append(w)
+        first_order.append(self.beta[reward_size - 1])
+        first_order_weights = th.tensor(first_order)
+
+        # compute ppo loss
+        with th.no_grad():
+            baseline = self.agents[k].critic(states.to(self.device))
+            outcome = rewards + (self.discount * self.agents[k].critic(next_states.to(self.device)) * (1 - dones))
+            advantage = (outcome - baseline).detach()
 
