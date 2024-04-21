84833d1d3dc6d0147c0286871288c38506ddf5d9
Núria Torquet Luna <ntorqulu11@alumnes.ub.edu>
2024-04-21 22:17:44 +0200

    checking if i can merge now

--- src/train.py
+++ src/train.py
@@ -10,13 +10,13 @@ from callbacks import PrintAverageReward, TensorBoardLogging
 
 if __name__ == '__main__':
     # Create the environment
-    # scalarised or vectorized
-    tiny["reward_mode"] = "scalarised"
+    # scalarised or vectorial
+    tiny["reward_mode"] = "vectorial"
     env = gym.make("MultiAgentEthicalGathering-v1", **tiny)
     # Set parameters
     params = TrainingParameters(env_name="MultiAgentEthicalGathering-v1")
     # initialize lexico_ppo agent
-    ppo = PPO(train_params=params, env=env)
+    ppo = LexicoPPO(train_params=params, env=env)
     ppo.addCallbacks(PrintAverageReward(ppo, 300))
     ppo.addCallbacks(TensorBoardLogging(ppo, f"StoreNuria/tensorboard_logs/{ppo.run_name}"))
     ppo.train()
